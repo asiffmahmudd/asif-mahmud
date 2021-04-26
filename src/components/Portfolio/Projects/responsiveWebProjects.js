0d4e2c0c53b10c901bffe4447678941d2b802cfb
Asif Mahmud <asifmahmud3472@gmail.com>
2021-04-27 03:09:44 +0600

    all projects data added

--- src/components/Portfolio/Projects/responsiveWebProjects.js
+++ src/components/Portfolio/Projects/responsiveWebProjects.js
@@ -1,15 +1,15 @@
 
-import todo from '../../../img/Javascript-express/todo.png';
-import simon from '../../../img/Javascript-express/simon.png';
-import drum from '../../../img/Javascript-express/drum.png';
-import dice from '../../../img/Javascript-express/dice.png';
-import blog from '../../../img/Javascript-express/blog.png';
-import slider from '../../../img/Javascript-express/slider.png';
-import issue from '../../../img/Javascript-express/issue.png';
-import rock from '../../../img/Javascript-express/rock.png';
-import hungry from '../../../img/Javascript-express/hungry.png';
-import country from '../../../img/Javascript-express/country.png';
-import weather from '../../../img/Javascript-express/weather.png';
+import nike from '../../../img/ResponsiveWeb/nike.png';
+import cashcars from '../../../img/ResponsiveWeb/cashcars.png';
+import confusion from '../../../img/ResponsiveWeb/confusion.png';
+import cuda from '../../../img/ResponsiveWeb/cuda.png';
+import elongation from '../../../img/ResponsiveWeb/elongation.png';
+import gadgets from '../../../img/ResponsiveWeb/gadgets.png';
+import mache from '../../../img/ResponsiveWeb/mache.png';
+import panakeia from '../../../img/ResponsiveWeb/panakeia.png';
+import school from '../../../img/ResponsiveWeb/school.png';
+import strict from '../../../img/ResponsiveWeb/strict.png';
+import tanatos from '../../../img/ResponsiveWeb/tanatos.png';
 
 const responsiveWebProjects = [
     {
@@ -21,11 +21,83 @@ const responsiveWebProjects = [
         desc: ""
     },
     {
-        title: "Nike Store",
-        img: nike,
-        live: "https://asiffmahmudd.github.io/nike-react/",
-        gitClient: "https://github.com/asiffmahmudd/nike-react",
+        title: "Cash for Cars",
+        img: cashcars,
+        live: "https://asiffmahmudd.github.io/Responsive-Web/Cars-for-cash/",
+        gitClient: "https://github.com/asiffmahmudd/Responsive-Web/tree/master/Cars-for-cash",
         technologies: "HTML, CSS, Bootstrap",
+        desc: ""
+    },
+    {
+        title: "Restorante Confusion",
+        img: confusion,
+        live: "https://asiffmahmudd.github.io/Responsive-Web/Confusion/",
+        gitClient: "https://github.com/asiffmahmudd/Responsive-Web/tree/master/Confusion",
+        technologies: "HTML, CSS, Bootstrap",
+        desc: ""
+    },
+    {
+        title: "Cuda",
+        img: cuda,
+        live: "https://asiffmahmudd.github.io/Responsive-Web/Cuda/",
+        gitClient: "https://github.com/asiffmahmudd/Responsive-Web/tree/master/Cuda",
+        technologies: "HTML, CSS, Bootstrap, Wow JS",
+        desc: ""
+    },
+    {
+        title: "Elongation",
+        img: elongation,
+        live: "https://asiffmahmudd.github.io/Responsive-Web/Elongation/",
+        gitClient: "https://github.com/asiffmahmudd/Responsive-Web/tree/master/Elongation",
+        technologies: "HTML, CSS, Bootstrap",
+        desc: ""
+    },
+    {
+        title: "Hot Gadgets",
+        img: gadgets,
+        live: "https://asiffmahmudd.github.io/Responsive-Web/E-Commerce/",
+        gitClient: "https://github.com/asiffmahmudd/Responsive-Web/tree/master/E-Commerce",
+        technologies: "HTML, CSS, Bootstrap, Wow JS",
+        desc: ""
+    },
+    {
+        title: "Mache",
+        img: mache,
+        live: "https://asiffmahmudd.github.io/Responsive-Web/Mache/",
+        gitClient: "https://github.com/asiffmahmudd/Responsive-Web/tree/master/Mache",
+        technologies: "HTML, CSS, Bootstrap, Wow JS",
+        desc: ""
+    },
+    {
+        title: "Panakeia",
+        img: panakeia,
+        live: "https://asiffmahmudd.github.io/Responsive-Web/Panekeia/",
+        gitClient: "https://github.com/asiffmahmudd/Responsive-Web/tree/master/Panekeia",
+        technologies: "HTML, CSS, Bootstrap, Wow JS",
+        desc: ""
+    },
+    {
+        title: "Online School",
+        img: school,
+        live: "https://asiffmahmudd.github.io/Responsive-Web/Online%20School/",
+        gitClient: "https://github.com/asiffmahmudd/Responsive-Web/tree/master/Online%20School",
+        technologies: "HTML, CSS, Bootstrap, Wow JS",
+        desc: ""
+    },
+    {
+        title: "Strict",
+        img: strict,
+        live: "https://asiffmahmudd.github.io/Responsive-Web/Strict/",
+        gitClient: "https://github.com/asiffmahmudd/Responsive-Web/tree/master/Strict",
+        technologies: "HTML, CSS, Bootstrap, Wow JS",
+        desc: ""
+    },
+    {
+        title: "Tanatos",
+        img: tanatos,
+        live: "https://asiffmahmudd.github.io/Responsive-Web/Tanatos/",
+        gitClient: "https://github.com/asiffmahmudd/Responsive-Web/tree/master/Tanatos",
+        technologies: "HTML, CSS, Bootstrap, Wow JS",
         desc: ""
     },
     
---
+++ src/components/Portfolio/Projects/wordpress.js
@@ -0,0 +1,85 @@
+import agency from '../../../img/Wordpress/agency.png';
+import assurance from '../../../img/Wordpress/assurance.png';
+import digimedia from '../../../img/Wordpress/digimedia.png';
+import gkjauto from '../../../img/Wordpress/gkjauto.png';
+import heyu from '../../../img/Wordpress/heyu.png';
+import newsletter from '../../../img/Wordpress/newsletter.png';
+import nextdoor from '../../../img/Wordpress/nextdoor.png';
+import onlinebiz from '../../../img/Wordpress/onlinebiz.png';
+import peskero from '../../../img/Wordpress/peskero.png';
+import webinar from '../../../img/Wordpress/webinar.png';
+
+const responsiveWebProjects = [
+    {
+        title: "Business Agency",
+        img: agency,
+        live: "http://agency.ezyro.com/?i=1",
+        technologies: "Wordpress, Elementor",
+        desc: ""
+    },
+    {
+        title: "Assurance Valeur Plus",
+        img: assurance,
+        live: "http://assurancevaleurplus.com/",
+        technologies: "Wordpress, WPML, Ninja Form, Elementor",
+        desc: ""
+    },
+    {
+        title: "Digimedia",
+        img: digimedia,
+        live: "http://digimedia.ezyro.com/?i=1",
+        technologies: "Wordpress, Divi",
+        desc: ""
+    },
+    {
+        title: "GKJ Auto",
+        img: gkjauto,
+        live: "https://gkjauto.com/en/",
+        technologies: "Wordpress, WPML, WP Bakery",
+        desc: ""
+    },
+    {
+        title: "HeyU",
+        img: heyu,
+        live: "http://heyu.ezyro.com/?i=1",
+        technologies: "Wordpress, Elementor",
+        desc: ""
+    },
+    {
+        title: "Newsletter",
+        img: newsletter,
+        live: "http://newsletter.ezyro.com/?i=1",
+        technologies: "Wordpress, Elementor",
+        desc: ""
+    },
+    {
+        title: "Next Door Event",
+        img: nextdoor,
+        live: "http://nextdoorevent.ezyro.com/?i=1",
+        technologies: "Wordpress, Elementor",
+        desc: ""
+    },
+    {
+        title: "Online Biz",
+        img: onlinebiz,
+        live: "http://onlinebiz.ezyro.com/?i=1",
+        technologies: "Wordpress, Contact Form 7",
+        desc: ""
+    },
+    {
+        title: "Restuarante Peskero",
+        img: peskero,
+        live: "https://dev.peskero.com/",
+        technologies: "Wordpress, WPML, Contact Form 7, Elementor",
+        desc: ""
+    },
+    {
+        title: "Webinar",
+        img: webinar,
+        live: "http://webinar.ezyro.com/",
+        technologies: "Wordpress, Elementor",
+        desc: ""
+    },
+]
+
+export default responsiveWebProjects;
