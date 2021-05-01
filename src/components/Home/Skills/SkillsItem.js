e98cad795ba96da1b346f42f65c815efeb628870
Asif Mahmud <asifmahmud3472@gmail.com>
2021-05-01 17:04:56 +0600

    chnaged icons

--- src/components/Home/Skills/SkillsItem.js
+++ src/components/Home/Skills/SkillsItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 
 const SkillsItem = ({skillinfo}) => {
     return (
-            <div className="col-xl-2 col-md-3 col-sm-3 col-4 col-xs-6 mt-3 skill-items text-center">
-                <FontAwesomeIcon icon={skillinfo.icon} color="white" size="4x"></FontAwesomeIcon>
-                <h4 className="text-center text-white mt-2">{skillinfo.title}</h4>
+            <div className="col-xl-2 col-md-3 col-sm-3 col-4 col-xs-6 mt-5 skill-items text-center">
+                
+                {/* <FontAwesomeIcon icon={skillinfo.icon} color="white" size="4x"></FontAwesomeIcon> */}
+                {skillinfo.icon}
+                {/* <h4 className="text-center text-white mt-2">{skillinfo.title}</h4> */}
             </div>
     );
 };
