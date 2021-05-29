753b0fe4f1e358ddbd5dea9024955d6d0a07880a
Asif Mahmud <asifmahmud3472@gmail.com>
2021-05-29 19:23:29 +0600

    added styles

--- src/components/Home/Blog/BlogItem.js
+++ src/components/Home/Blog/BlogItem.js
@@ -5,7 +5,9 @@ const BlogItem = ({blogItem}) => {
     return (
         <div className="blog-item">
             <div className="card shadow" style={{width:"18rem"}}>
-                <img className="card-img-top img-fluid" src={blogItem.img} style={{height:"230px"}} alt="" />
+                <div className="card-img-container">
+                    <img className="card-img-top img-fluid" src={blogItem.img} style={{height:"230px"}} alt="" />
+                </div>
                 <div className="card-body" style={{height:"250px"}}>
                     <h5 className="card-title" style={{height:"20%"}}>{blogItem.title}</h5>
                     <p className="card-text">{blogItem.desc}</p>
