77852489fbe739ad21810129e71f025708ca3a2e
Asif Mahmud <asifmahmud3472@gmail.com>
2023-02-17 10:42:25 -0500

    updated projects

--- src/components/Portfolio/Projects/reactProjects.js
+++ src/components/Portfolio/Projects/reactProjects.js
@@ -8,7 +8,9 @@ import volunteer from '../../../img/recentWorks/volunteer-network.png';
 import bookShop from '../../../img/React/bookShop.png';
 import burj from '../../../img/React/burj.png';
 import reduxBookshelf from '../../../img/recentWorks/redux-bookshelf.png';
-import pickbazar from '../../../img/recentWorks/pickbazar.JPG'
+import pickbazar from '../../../img/recentWorks/pickbazar.JPG';
+import playerInfo from '../../../img/React/player-info.png';
+import toDo from '../../../img/React/to-do.png';
 
 const reactProjects = [
     {
@@ -28,48 +30,6 @@ const reactProjects = [
         gitServer: "https://github.com/asiffmahmudd/redux-bookshelf-server",
         technologies: "React, Redux, Express, NodeJs, MongoDB, Bootstrap, JSX, CSS, Firebase, Heroku",
         desc: "A bookshelf management system. You can add different books on different lists depending on the status."
-    },
-    {
-        title: "Rest Countries",
-        img: restCountries,
-        live: "https://boring-aryabhata-445861.netlify.app/",
-        gitClient: "https://github.com/asiffmahmudd/rest-countries",
-        technologies: "Rest Countries API, React, HTML, CSS, Bootstrap",
-        desc: "You can search for different countries and see their infos"
-    },
-    {
-        title: "Ema-John",
-        img: emaJohn,
-        live: "https://ema-john-10238.web.app/",
-        gitClient: "https://github.com/asiffmahmudd/ema-john-simple",
-        gitServer: "https://github.com/asiffmahmudd/ema-john-server",
-        technologies: "React, Express, HTML, CSS, Bootstrap, Firebase, MongoDB",
-        desc: "An e-commerce site where you can buy different gadgets and accessories"
-    },
-    {
-        title: "Team Tracker",
-        img: teamTracker,
-        live: "https://quirky-franklin-067139.netlify.app/",
-        gitClient: "https://github.com/asiffmahmudd/club-info",
-        technologies: "React, HTML, CSS, Bootstrap",
-        desc: "You can find different football clubs and their info in here"
-    },
-    {
-        title: "Transport Fare",
-        img: transportFare,
-        live: "https://jovial-beaver-bf6a7a.netlify.app/",
-        gitClient: "https://github.com/asiffmahmudd/transport-fare",
-        technologies: "React, HTML, CSS, Bootstrap, Firebase, MongoDB",
-        desc: "A site where you can find the fare of different vehicles depending on the routes in Dhaka"
-    },
-    {
-        title: "Trip Advisor",
-        img: tripAdvisor,
-        live: "https://travel-agency-a2b4d.web.app/",
-        gitClient: "https://github.com/asiffmahmudd/travel-agency",
-        gitServer: "https://github.com/asiffmahmudd/travel-agency-server",
-        technologies: "React, Express, HTML, CSS, Bootstrap, Firebase, MongoDB, stripe",
-        desc: "This site gives advice on trips planning and other stuffs. You can book an appointment for a particular service and see your bookings on the user panel"
     },
     {
         title: "Volunteer Network",
@@ -97,6 +57,64 @@ const reactProjects = [
         gitServer: "https://github.com/asiffmahmudd/burj-al-arab-server",
         technologies: "React, Express, HTML, CSS, Bootstrap, Firebase, MongoDB",
         desc: "A site to book hotels"
+    },
+    {
+        title: "Ema-John",
+        img: emaJohn,
+        live: "https://ema-john-10238.web.app/",
+        gitClient: "https://github.com/asiffmahmudd/ema-john-simple",
+        gitServer: "https://github.com/asiffmahmudd/ema-john-server",
+        technologies: "React, Express, HTML, CSS, Bootstrap, Firebase, MongoDB",
+        desc: "An e-commerce site where you can buy different gadgets and accessories"
+    },
+    // {
+    //     title: "Rest Countries",
+    //     img: restCountries,
+    //     live: "https://boring-aryabhata-445861.netlify.app/",
+    //     gitClient: "https://github.com/asiffmahmudd/rest-countries",
+    //     technologies: "Rest Countries API, React, HTML, CSS, Bootstrap",
+    //     desc: "You can search for different countries and see their infos"
+    // },
+    {
+        title: "To Do List",
+        img: toDo,
+        live: "https://friendly-swanson-abf7a4.netlify.app/",
+        gitClient: "https://github.com/asiffmahmudd/typescript-todo-app",
+        technologies: "Typescript, React, HTML, CSS, Bootstrap, JavaScript",
+        desc: "Create your to do list"
+    },
+    {
+        title: "Player Market",
+        img: playerInfo,
+        live: "https://gifted-morse-2df3cc.netlify.app/",
+        gitClient: "https://github.com/asiffmahmudd/player-market",
+        technologies: "React, HTML, CSS, Bootstrap",
+        desc: "Buy players available from the market"
+    },
+    // {
+    //     title: "Team Tracker",
+    //     img: teamTracker,
+    //     live: "https://quirky-franklin-067139.netlify.app/",
+    //     gitClient: "https://github.com/asiffmahmudd/club-info",
+    //     technologies: "React, HTML, CSS, Bootstrap",
+    //     desc: "You can find different football clubs and their info in here"
+    // },
+    {
+        title: "Transport Fare",
+        img: transportFare,
+        live: "https://jovial-beaver-bf6a7a.netlify.app/",
+        gitClient: "https://github.com/asiffmahmudd/transport-fare",
+        technologies: "React, HTML, CSS, Bootstrap, Firebase, MongoDB",
+        desc: "A site where you can find the fare of different vehicles depending on the routes in Dhaka"
+    },
+    {
+        title: "Trip Advisor",
+        img: tripAdvisor,
+        live: "https://travel-agency-a2b4d.web.app/",
+        gitClient: "https://github.com/asiffmahmudd/travel-agency",
+        gitServer: "https://github.com/asiffmahmudd/travel-agency-server",
+        technologies: "React, Express, HTML, CSS, Bootstrap, Firebase, MongoDB, stripe",
+        desc: "This site gives advice on trips planning and other stuffs. You can book an appointment for a particular service and see your bookings on the user panel"
     }
 ]
 
